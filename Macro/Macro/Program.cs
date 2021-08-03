f615253d947afbc45df4063108c02d06c788b986
Kellen Stuart <kellen.m.stuart@gmail.com>
2021-08-02 19:36:44 -0600

    got a working copy

--- Macro/Macro/Program.cs
+++ Macro/Macro/Program.cs
@@ -1,30 +1,93 @@
-﻿using System;
+using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading;
+using System.Windows.Forms;
 
-namespace PathOfExile
+namespace Macro
 {
-    public class Program
+    static class Program
     {
-        const UInt32 WM_KEYDOWN = 0x0100;
-        const int VK_M = 0x4D;
-
-        [DllImport("user32.dll")]
-        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
-
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static Process _pathOfExileProcess;
+        private const string poeProcessName = "PathOfExile";
+        
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
-            while (true)
+            var processes = Process.GetProcessesByName(poeProcessName);
+            if (!processes.Any())
+                throw new Exception($"{poeProcessName} must be running to execute this macro.");
+
+            _pathOfExileProcess = processes.Single();
+            
+            _hookID = SetHook(_proc);
+            // Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+        
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process poeProcess = Process.GetProcessesByName("pathofexile").Single())
+            using (ProcessModule poeModule = poeProcess.MainModule)
             {
-                Process[] processes = Process.GetProcessesByName("notepad");
-
-                foreach (Process proc in processes)
-                    PostMessage(proc.MainWindowHandle, WM_KEYDOWN, VK_M, 0);
-
-                Thread.Sleep(5000);
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(poeModule.ModuleName), 0);
             }
         }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                #if DEBUG
+                Console.WriteLine((Keys)vkCode);
+                #endif
+                if ((Keys) vkCode == Keys.D4)
+                {
+                    SetForegroundWindow(_pathOfExileProcess.MainWindowHandle);
+                    SendKeys.SendWait("{1}{2}{3}{5}{q}{w}");
+                }
+
+                if ((Keys) vkCode == Keys.D6)
+                {
+                    
+                }
+                    
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
     }
 }
